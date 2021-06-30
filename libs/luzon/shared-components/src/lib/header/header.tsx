2580b738721bd0309aa3b8b14e028ebad3483e51
Alina <kansakaralina@gmail.com>
2021-06-30 20:02:45 +0545

    Changes made on header component

--- libs/luzon/shared-components/src/lib/header/header.tsx
+++ libs/luzon/shared-components/src/lib/header/header.tsx
@@ -6,7 +6,7 @@ export interface HeaderProps {}
 export function Header(props: HeaderProps) {
   return (
     <div>
-      <h1>Welcome to Header!</h1>
+      <h1>Welcome to Button!</h1>
     </div>
   );
 }
