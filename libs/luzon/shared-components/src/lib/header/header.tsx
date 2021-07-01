febd83ad2ba6daea85555751978f14e724807872
Bikeshrestha <bikesh@heubert.com>
2021-07-01 10:26:54 +0545

    New cahnegs made

--- libs/luzon/shared-components/src/lib/header/header.tsx
+++ libs/luzon/shared-components/src/lib/header/header.tsx
@@ -6,7 +6,7 @@ export interface HeaderProps {}
 export function Header(props: HeaderProps) {
   return (
     <div>
-      <h1>Welcome to Header!</h1>
+      <h1>Welcome to Lyra Project!</h1>
     </div>
   );
 }
